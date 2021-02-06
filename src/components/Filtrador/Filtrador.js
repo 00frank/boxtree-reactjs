4776c873ba264a6d47f66446b9735313fbe16759
Frank Garcia <00frankgarcia@gmail.com>
2021-02-06 15:09:36 -0300

    version estatica de la pagina

--- src/components/Filtrador/Filtrador.js
+++ src/components/Filtrador/Filtrador.js
@@ -7,14 +7,44 @@ class Filtrador extends React.Component {
       <div id="filtrador">
         <form>
           <input type="text" placeholder="Buscar..." />
-          <label>
+          <label className="switch">
             <input type="checkbox" />
-          Solo mostrar productos con stock
+            <span className="slider round"></span>
           </label>
+          <label className="message">Mostrar unicamente los productos con stock</label>
         </form>
       </div>
     )
   }
 }
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: this.props.active
+    }
+    // Este enlace es necesario para hacer que `this` funcione en el callback
+    this.handleClick = this.handleClick.bind(this);
+    // tambien se puede obviar el bindeo de this declarandolo como una arrow func. ej:
+    // handleClick = () => { ... }
+    // o llamar a la funcion con una arrow func. "onClick={() => this.handleClick()}", (no recomendado)
+  }
+  handleClick() {
+    this.setState({ active: !this.state.active })
+    // se ejecuta la funcion recibida del padre
+    this.props.handleShow(this.props.name, !this.state.active);
+  }
+  render() {
+    return (
+      <label className="switch">
+        <input type="checkbox" checked={this.state.active} onChange={this.handleClick} />
+        <span className="slider round"></span>
+      </label>
+      // <label>Mostrar {this.props.name}? ({this.state.active ? 'Si' : 'No'})</label>
+    )
+  }
+}
+
 
 export default Filtrador
--- src/components/PTable/PTable.js
+++ src/components/PTable/PTable.js
@@ -3,29 +3,55 @@ import './PTable.css'
 
 class PTable extends React.Component {
   render() {
+    const rows = []
+    let lastCategory = null
+    PRODUCTS.forEach(p => {
+      if(p.category !== lastCategory){
+        rows.push(
+          <tr key={p.category}>
+            <td colSpan="2" className="category">{p.category}</td>
+          </tr>
+        )
+      }
+      rows.push(
+          <ProductRow product={p} key={p.name} />
+      )
+      lastCategory = p.category;
+    })   
     return (
       <table id="productos">
-        <tr>
-          <th>Nombre</th>
-          <th>Precio</th>
-        </tr>
-        {PRODUCTS.map(e =>
-          <tr key={e.name}>
-            <td className={!e.stocked ? 'no-stock' : ''}>{e.name}</td>
-            <td>{e.price}</td>
-          </tr>)}
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Precio</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
       </table>
     )
   }
 }
 
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product
+    const name = product.stocked === true ? product.name : <span className="no-stock">{product.name}</span>
+    return(
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    )
+  }
+}
+
 const PRODUCTS = [
-  { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-  { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-  { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-  { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-  { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+  { category: 'Deportes', price: '$49.99', stocked: true, name: 'Football' },
+  { category: 'Deportes', price: '$9.99', stocked: true, name: 'Baseball' },
+  { category: 'Deportes', price: '$29.99', stocked: false, name: 'Basketball' },
+  { category: 'Electronica', price: '$99.99', stocked: true, name: 'iPod Touch' },
+  { category: 'Electronica', price: '$399.99', stocked: false, name: 'iPhone 5' },
+  { category: 'Electronica', price: '$199.99', stocked: true, name: 'Nexus 7' }
 ];
 
 export default PTable;
